c7eaf3122c81ceb9e5f01ec993b3603de7ddba02
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-03-23 19:46:41 -0700

    Added components and navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <Link to="/Home">HOME</Link>
+            <br />
+            <Link to="/RecentlyAdded">RECENTLY ADDED</Link>
+            <br />
+            <Link to="/ShopBySeries">SHOP BY SERIES</Link>
+            <br />
+            <Link to="/AboutUs">ABOUT US</Link>
+            <br />
+            <Link to="/Contact">CONTACT</Link>
+            <br />
+            <Link to="/TrackOrder">TRACK ORDER</Link>
+        </nav>
+    )
+}
