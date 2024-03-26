339baaf4fc8b97d48e455bfa50d1253429f30a9a
Chelsea Scott <chelseacat62@gmail.com>
2024-03-25 19:40:33 -0700

    account on hover functionality added

--- src/Components/AccountButtonPopup.jsx
+++ src/Components/AccountButtonPopup.jsx
@@ -1,9 +1,9 @@
 import "./CSS/Account.css"
 
-export default function AccountButtonPopup() {
+export default function AccountButtonPopup({hovered}) {
 
     return (
-        <div className="popupwrapper">
+        <div className={`popupwrapper ${!hovered? "notHovered":""}`}>
             <p>LOGIN</p>
             <hr />
             <p>SIGN UP</p>
