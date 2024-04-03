0833f77488b72227c84d50679eaaf96f23b90c7d
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-04-02 22:39:26 -0700

    need popup for cart functionality debugged

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,34 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AccountButton from './AccountButton'
+import AccountButton from "./AccountButton";
 import AddToCart from "./AddToCart";
+import CartPopup from "./CartPopup";
 
 export default function NavBar({ token, cartCount }) {
-    return (
-        <nav>
-            <Link to="/Home">HOME</Link>
-            <br />
-            <Link to="/RecentlyAdded">RECENTLY ADDED</Link>
-            <br />
-            <Link to="/ShowAllProducts">SHOW ALL PRODUCTS</Link>
-            <br />
-            <Link to="/AboutUs">ABOUT US</Link>
-            <br />
-            <Link to="/Contact">CONTACT</Link>
-            <br />
-            <Link to="/TrackOrder">TRACK ORDER</Link>
-            <br />
-            <AccountButton token={token}/>
-            <br />
-            <AddToCart cartCount={cartCount}/>
-        </nav>
-    )
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCartPopup = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  return (
+    <nav>
+      <Link to="/Home">HOME</Link>
+      <br />
+      <Link to="/RecentlyAdded">RECENTLY ADDED</Link>
+      <br />
+      <Link to="/ShowAllProducts">SHOW ALL PRODUCTS</Link>
+      <br />
+      <Link to="/AboutUs">ABOUT US</Link>
+      <br />
+      <Link to="/Contact">CONTACT</Link>
+      <br />
+      <Link to="/TrackOrder">TRACK ORDER</Link>
+      <br />
+      <AccountButton token={token} />
+      <br />
+      <AddToCart cartCount={cartCount} onClick={toggleCartPopup} />
+      {isCartOpen && <CartPopup />}
+    </nav>
+  );
 }
