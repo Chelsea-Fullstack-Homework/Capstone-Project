bb5c3426406cb11b3b1d137fba14e9410b147d52
Chelsea Scott <chelseacat62@gmail.com>
2024-04-03 21:10:57 -0700

    added user table. created get api/book. created post api/users/register

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const manga = require("./manga")
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
+
 // imports here for express and pg
 const express = require('express');
 const app = express();
@@ -6,18 +9,57 @@ const path = require('path');
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/manga_db')
 
+app.use(express.json())
+
 // static routes here (you only need these for deployment)
 
-// app routes here 
-app.get('/api/manga', async (req, res) => {
+// app routes
+app.get('/api/books', async (req, res) => {
     const SQL = `
-    SELECT * FROM manga_db;
+    SELECT * FROM manga;
     `;
     const response = await client.query(SQL);
     res.json(response.rows);
 });
 
-// create your init function
+app.post('/api/users/register', async (req, res) => {
+
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const token = jwt.sign({
+        data: { firstname, lastname, email, password }
+    }, crypto.randomBytes(64).toString('hex'));
+      
+    const SQL = `
+    INSERT INTO users(firstname, lastname, email, password, token)
+    VALUES($1,$2,$3,$4,$5)
+    RETURNING *;
+    `;
+    try {
+        const response = await client.query(SQL, [firstname, lastname, email, password, token]);
+        const user = response.rows[0];
+        res.send({
+            "user": {
+                "id": user.id,
+                "firstname": user.firstname,
+                "lastname": user.lastname,
+                "email": user.email
+            },
+            "message": "Registration successful!",
+            "token": user.token
+        });
+    } catch (err) {
+        console.error(err);
+        res.send({
+            "message": "Registration failed!"
+        });
+    }
+});
+
+// create init function
 const init = async()=>{
     await client.connect();
 
@@ -30,8 +72,19 @@ const init = async()=>{
      */
 
     let SQL = `
-    DROP TABLE IF EXISTS manga_db;
-    CREATE TABLE manga_db(
+    DROP TABLE IF EXISTS manga;
+    DROP TABLE IF EXISTS users;
+
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        firstname text,
+        lastname text,
+        email text NOT NULL UNIQUE,
+        password text NOT NULL,
+        token text NOT NULL UNIQUE
+    );
+
+    CREATE TABLE manga(
         sku SERIAL PRIMARY KEY,
         title VARCHAR(255),
         author VARCHAR(255),
@@ -45,7 +98,7 @@ const init = async()=>{
     console.log('tables created');
 
     SQL = `
-    INSERT INTO manga_db 
+    INSERT INTO manga 
         (title, author, price, coverimage)
     values($1,$2,$3,$4)
     `;
