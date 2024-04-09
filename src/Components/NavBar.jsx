cc91662ae518999e126f4da66207b461f80ecd3e
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-04-08 21:25:30 -0700

    added search bar (not functional)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,6 +12,17 @@ export default function NavBar({ token, cartCount }) {
     setIsCartOpen(!isCartOpen);
   };
 
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    const searchTerm = event.target.elements.searchInput.value.trim();
+    if (searchTerm !== "") {
+      alert("Searching for: " + searchTerm);
+    } else {
+      alert("Please enter a search term");
+    }
+    event.target.elements.searchInput.value = "";
+  };
+
   return (
     <nav className="navbar">
       <ul>
@@ -25,7 +36,12 @@ export default function NavBar({ token, cartCount }) {
         <li><AddToCart cartCount={cartCount} onClick={toggleCartPopup} /></li>
       </ul>
       {isCartOpen && <Cart />}
+      <form onSubmit={handleSearchSubmit} className="search-bar">
+        <input type="text" name="searchInput" placeholder="Search..." className="search-input" />
+        <button type="submit" className="search-button">
+          <img src="https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_1280.png" alt="Search" className="search-icon" />
+        </button>
+      </form>
     </nav>
   );
-}
-
+};
