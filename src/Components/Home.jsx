55160cf3428d43b82804907a4834223655487a14
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-04-09 21:25:03 -0700

    added home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,18 @@
+// Home.jsx
 import React from "react";
 import { Link } from "react-router-dom";
+import "./CSS/Home.css"
 
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>Welcome to Our Website!</h1>
-            <p>We are excited to have you here. Browse our collection of books and enjoy reading!</p>
-            <div className="explore-links">
-                <Link to="/ShowAllProducts">Explore Books</Link>
-                <Link to="/Contact">Contact Us</Link>
+            <div className="background-image">
+                <h1>Welcome to Our Website!</h1><br />
+                <p>We are excited to have you here. Browse our collection of manga and enjoy reading!</p>
+                <div className="explore-links">
+                    <Link to="/ShowAllProducts">Explore Manga</Link>
+                    <Link to="/Contact">Contact Us</Link>
+                </div>
             </div>
         </div>
     );
