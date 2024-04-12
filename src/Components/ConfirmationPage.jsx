25214d1d3d481b54304fcc294709745bea34c8fb
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-04-11 17:34:20 -0700

    added confirmation page pictures

--- src/Components/ConfirmationPage.jsx
+++ src/Components/ConfirmationPage.jsx
@@ -29,6 +29,9 @@ export default function ConfirmationPage() {
       <p>Your order number is: <Link to="/TrackOrder" className="order-number-link">{orderNumber}</Link></p>
       <p>We've sent a confirmation email to your registered email address.</p>
       <Link to="/ShowAllProducts" className="shop-more-button">Shop More</Link>
+      <img src="https://www.pngall.com/wp-content/uploads/14/Killua-PNG-Pic.png" className="conf-logo"></img>
+      <img src="https://blood-fashion-chainsaw.carrd.co/assets/images/image02.png?v=0176b510" className="conf-logo2"></img>
+      <img src="https://asia.sega.com/kimetsu_hinokami/en/assets/img/sub/character/chara3_thumb.png" className="conf-logo3"></img>
     </div>
   );
 }
